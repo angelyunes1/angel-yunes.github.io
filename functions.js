8d2364af7cb1c2fd5cad716fcd5de8491598a1cb
Angel Yunes <111542942+angelyunes1@users.noreply.github.com>
2023-03-18 19:56:12 -0300

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,4 @@
+document.getElementById('boton2').addEventListener('click',function(){
+    console.log("Hola mundo desde EventListener");
+    document.getElementById("frase1").innerHTML ="Mail:angelyunes1@hotmail.com";   
+})
